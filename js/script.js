a2f947fc1df930b4aa40dd0a21899eb9c17f61f8
Vinay <vinay.g2404@gmail.com>
2022-01-07 21:51:33 +0530

    Product Page and Modification in other pages

--- js/script.js
+++ js/script.js
@@ -36,6 +36,24 @@ var swiper = new Swiper(".mySwiper", {
 	},
 });
 
+// Product Swiper
+var swiper = new Swiper(".mySwiper3", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
 // Wow
 new WOW().init();
 
